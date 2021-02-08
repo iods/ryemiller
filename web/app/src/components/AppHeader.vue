e1f48550e0122d65d44cfb36aa45ad91669e2bc1
Rye Miller <millerrye17@gmail.com>
2021-02-08 04:42:03 -0700

    Update to menu positioning for navigation.

--- web/app/src/components/AppHeader.vue
+++ web/app/src/components/AppHeader.vue
@@ -16,7 +16,7 @@
             <span class="toggler-icon"></span>
             <span class="toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse hidden lg:block duration-300 shadow absolute top-100 left-0 mt-full bg-white z-20 px-5 py-3 w-full lg:static lg:bg-transparent lg:shadow-none" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse hidden lg:block duration-300 shadow absolute top-10 left-0 mt-full bg-white z-20 px-5 py-3 w-full lg:static lg:bg-transparent lg:shadow-none" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto justify-center items-center lg:flex">
               <li class="nav-item">
                 <a class="page-scroll active" href="#hero">Home</a>
--- web/app/src/components/AppHero.vue
+++ web/app/src/components/AppHero.vue
@@ -6,12 +6,12 @@
 
     <div class="container">
       <div class="w-full max-w-2xl mx-auto">
-        <span class="text-sm font-semibold iods">Rye Miller</span>
+        <span class="text-sm font-semibold iods">My name is Rye Miller.</span>
         <h2 class="text-5xl mt-2 mb-6 leading-tight font-semibold font-heading">Software Developer</h2>
         <p class="mb-8 text-gray-700 leading-relaxed">There will be a bunch of text here that I will add and put in here to make myself sound really cool and really important
           I just do not know what to add here yet since I am building the site.</p>
         <div>
-          <a class="inline-block hover:underline pb-8" href="#">View Some Projects »</a>
+          <a class="inline-block hover:underline pb-8 page-scroll" href="#projects">View Some Projects »</a>
         </div>
       </div>
     </div>
