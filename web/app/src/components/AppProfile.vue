1da8d4d031a22f37905077c2c709d167e6f25a52
Rye Miller <millerrye17@gmail.com>
2021-02-07 23:26:13 -0700

    Addition of Profile template for SPA page.

---
+++ web/app/src/components/AppProfile.vue
@@ -0,0 +1,76 @@
+<template>
+  <!--
+    PROFILE
+  -->
+  <section class="container">
+
+    <div class="py-12 lg:px-12 md:px-6">
+      <div class="mx-auto">
+        <div class="flex flex-wrap px-8">
+          <div class="w-full lg:w-1/3 lg:px-4 md:px-2 py-6">
+            <img src="../assets/img/code_3.svg" alt="" class="h-32 w-full hover:opacity-50">
+            <div class="px-6 py-4 border-b">
+              <div class="text-xl text-center iods font-bold">Cloud Architecture</div>
+            </div>
+            <p class="text-base text-center text-gray-800 pt-4">I find value in cost-effective, high-performing, resilient, and secure architectures. Oh and by the way, I like to build them too.</p>
+            <div class="text-center">
+              <div class="iods pt-4">Solutions I Architect:</div>
+              <p class="py-2">Application Stacks, Pipelines, Cloud Monitoring</p>
+              <div class="iods pt-3">Platforms &amp; Services:</div>
+              <ul class="py-2">
+                <li>Google Cloud Platform</li>
+                <li>BitBucket Pipelines</li>
+                <li>Github Actions</li>
+                <li>Docker</li>
+                <li>Rancher</li>
+              </ul>
+            </div>
+          </div>
+          <div class="w-full lg:w-1/3 lg:px-4 md:px-2 py-6">
+            <img src="../assets/img/code_1.svg" alt="" class="h-32 rounded-lg w-full hover:opacity-50">
+            <div class="px-6 py-4 border-b">
+              <div class="text-xl text-center iods font-bold">Software Development</div>
+            </div>
+            <p class="text-base text-center text-gray-800 pt-4">I specialize in application written in PHP with a focus in Magento. Recently, I have become a huge fan of Go and building CLI tools.</p>
+            <div class="text-center">
+              <div class="iods pt-4">Languages I Speak:</div>
+              <p class="py-2">PHP, JavaScript, Go, Python, Bash, SQL</p>
+              <div class="iods pt-3">Tools &amp; Environment:</div>
+              <ul class="py-2">
+                <li>JetBrains IDEs</li>
+                <li>Ubuntu 18.04</li>
+                <li>ohmyzsh</li>
+                <li>Github</li>
+                <li>Atom</li>
+              </ul>
+            </div>
+          </div>
+          <div class="w-full lg:w-1/3 lg:px-4 md:px-2 py-6">
+            <img src="../assets/img/evangelism.svg" alt="" class="h-32 rounded-lg w-full hover:opacity-25">
+            <div class="px-6 py-4 border-b">
+              <div class="text-xl text-center iods font-bold">Developer Evangelism</div>
+            </div>
+            <p class="text-base text-center text-gray-800 pt-4">I have a passion for bringing clarity and simplicity to the development process and the software teams behind it.</p>
+            <div class="text-center">
+              <div class="iods pt-4">Philosophies I Support:</div>
+              <p class="py-2">Mentorship, Community, Feedback</p>
+              <div class="iods pt-3">Roles &amp; Responsibilities:</div>
+              <ul class="py-2">
+                <li>Development Manager</li>
+                <li>Lead Developer</li>
+                <li>Educator</li>
+                <li>Mentor</li>
+                <li>Student</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <!--
+    PROFILE END
+  -->
+</template>
+
