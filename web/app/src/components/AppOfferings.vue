72d479e5b5fc1d4ca6004e332c2e2fc4b552c07e
Rye Miller <millerrye17@gmail.com>
2021-02-09 21:34:44 -0700

    Update of language for offerings blocks.

--- web/app/src/components/AppOfferings.vue
+++ web/app/src/components/AppOfferings.vue
@@ -5,7 +5,7 @@
   <section id="offerings" class="py-14 bg-gray-100">
 
     <div class="container">
-      <h2 class="text-3xl leading-tight mb-6 font-heading iods text-center">Here are my Offerings:</h2>
+      <h2 class="text-3xl leading-tight mb-6 font-heading iods text-center">Here are my offerings:</h2>
       <div class="flex flex-wrap justify-center md:justify-start lg:px-24">
 
         <div class="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3">
@@ -16,15 +16,14 @@
             <div class="mb-5">
               <p class="text-gray-700">
                 <span class="font-bold text-2xl">$45.00</span>
-                <span class="font-medium text-sm"> / starting</span>
+                <span class="font-medium text-sm"> / starting hourly</span>
               </p>
             </div>
             <ul class="mb-12">
-              <li class="text-gray-500 leading-9">Project Audit</li>
               <li class="text-gray-500 leading-9">Development</li>
-              <li class="text-gray-500 leading-9">Cloud Services</li>
+              <li class="text-gray-500 leading-9">Project Audit</li>
             </ul>
-            <a href="#" class="btn">Quote</a>
+            <a href="#" class="btn">Quote Me!</a>
           </div>
         </div>
 
@@ -36,16 +35,15 @@
             <div class="mb-5">
               <p class="text-gray-700">
                 <span class="font-bold text-2xl">$60.00</span>
-                <span class="font-medium text-sm"> / starting</span>
+                <span class="font-medium text-sm"> / starting hourly</span>
               </p>
             </div>
             <ul class="mb-12">
-              <li class="text-gray-500 leading-9">Project Audit</li>
-              <li class="text-gray-500 leading-9">Development</li>
               <li class="text-gray-500 leading-9">Cloud Services</li>
-              <li class="text-gray-500 leading-9">Contract Support</li>
+              <li class="text-gray-500 leading-9">Development</li>
+              <li class="text-gray-500 leading-9">Project Audit</li>
             </ul>
-            <a href="#" class="btn">Quote</a>
+            <a href="#" class="btn">Quote Me!</a>
           </div>
         </div>
 
@@ -57,17 +55,16 @@
             <div class="mb-5">
               <p class="text-gray-700">
                 <span class="font-bold text-2xl">$85.00</span>
-                <span class="font-medium text-sm"> / starting</span>
+                <span class="font-medium text-sm"> / starting hourly</span>
               </p>
             </div>
             <ul class="mb-12">
-              <li class="text-gray-500 leading-9">Project Audit</li>
-              <li class="text-gray-500 leading-9">Development</li>
               <li class="text-gray-500 leading-9">Cloud Services</li>
-              <li class="text-gray-500 leading-9">Jira Audit</li>
-              <li class="text-gray-500 leading-9">Contract Support</li>
+              <li class="text-gray-500 leading-9">Development</li>
+              <li class="text-gray-500 leading-9">Project Audit</li>
+              <li class="text-gray-500 leading-9">Jira Support</li>
             </ul>
-            <a href="#" class="btn">Quote</a>
+            <a href="#" class="btn">Quote Me!</a>
           </div>
         </div>
 
